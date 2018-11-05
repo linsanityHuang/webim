e9b2ebaf84414fffd2f9136d8a997218dcba49ed
黄建生 <js_huang@foxmail.com>
2018-11-05 12:29:41 +0800

    支持wss

--- chat/routing.py
+++ chat/routing.py
@@ -7,6 +7,6 @@ from . import consumers
 # 路由，指定 websocket 链接对应的 consumer
 websocket_urlpatterns = [
 	url(r'^wss/chat/(?P<room_name>[^/]+)/$', consumers.ChatConsumer),
-	url(r'^ws/chat/(?P<room_name>[^/]+)/$', consumers.ChatConsumer),
+	# url(r'^ws/chat/(?P<room_name>[^/]+)/$', consumers.ChatConsumer),
 	# path('ws/chat/<str:room_name>/', consumers.ChatConsumer),
 ]
